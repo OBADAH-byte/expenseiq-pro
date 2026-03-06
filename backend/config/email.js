dd9dc2cf59e477f698e456989ae240af3011fd6c
Obadah Furquan <obadah964@gmail.com>
2026-03-06 20:51:27 +0530

    Switch to port 2525

--- backend/config/email.js
+++ backend/config/email.js
@@ -1,14 +1,18 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 const nodemailer = require('nodemailer');
 
 const createTransporter = () => {
   return nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
+    host: 'smtp-relay.brevo.com',
+    port: 2525,
     secure: false,
+    tls: {
+      rejectUnauthorized: false
+    },
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
-    },
+    }
   });
 };
 
